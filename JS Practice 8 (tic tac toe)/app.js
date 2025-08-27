86a127f7ef002f670f769ff57087148f590292ed
Mukul Sharnagat <122820735+Mukulsharnagat01@users.noreply.github.com>
2025-08-28 01:08:27 +0530

    Add files via upload

---
+++ JS Practice 8 (tic tac toe)/app.js
@@ -0,0 +1,75 @@
+let boxes = document.querySelectorAll(".box"); 
+let resetBtn = document.querySelector("#resetBtn");
+let newgameBtn = document.querySelector("#new-btn");
+let msgcontainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true;//playerX, palyerO
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgcontainer.classList.add("hide");
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+        } else{
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    });
+});
+
+const disableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulation, Winner is ${winner}`;
+    msgcontainer.classList.remove("hide");
+    disableBoxes();
+};
+
+const checkWinner = () => {
+    for(let pattern of winPatterns) {
+            let pos1VAl = boxes[pattern[0]].innerText; 
+            let pos2VAl = boxes[pattern[1]].innerText; 
+            let pos3VAl = boxes[pattern[2]].innerText;
+
+            if(pos1VAl != "" && pos2VAl != "" && pos3VAl != "") {
+                if(pos1VAl === pos2VAl && pos2VAl === pos3VAl) {
+                    showWinner (pos1VAl);
+                }
+            }
+    }
+};
+
+newgameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
